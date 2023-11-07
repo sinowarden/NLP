0365793d40f7766b7a7a447ef9a8950f1d6b6ae9
sinowarden <sinowarden@gmail.com>
2023-11-07 18:51:05 +0800

    2023-11-07 18:51:05

--- core.py
+++ core.py
@@ -1,10 +1,10 @@
 
-            # 这是一个自动生成的Python文件
-            def hello_world():
-                print("Hello, world! Time is "Tue Nov  7 18:49:27 2023"")
+        # 这是一个自动生成的Python文件
+        def hello_world():
+            print("Hello, world! Time is 'Tue Nov  7 18:51:05 2023'")
 
 
-            if __name__ == "__main__":
-                hello_world()
-                b = 1
+        if __name__ == "__main__":
+            hello_world()
+            a = 1
             
