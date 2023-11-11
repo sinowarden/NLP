6ff6605f62223349af9ba439167d474d9bda8092
sinowarden <sinowarden@gmail.com>
2023-11-12 00:49:37 +0800

    2023-11-12 00:49:37

--- main.py
+++ main.py
@@ -9,3 +9,4 @@ if __name__ == "__main__":
     a = 1
             // Licensed under the Apache License, Version 2.0 (the 'License');
 // you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
