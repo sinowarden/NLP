f099df15903e34defec58233ab4783c2b584677f
sinowarden <sinowarden@gmail.com>
2023-11-12 09:34:24 +0800

    2023-11-12 09:34:24

--- main.py
+++ main.py
@@ -11,3 +11,4 @@ if __name__ == "__main__":
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
+//     http://www.apache.org/licenses/LICENSE-2.0
