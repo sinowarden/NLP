3ad5f321e376ed23cb2332982c43df789220db99
sinowarden <sinowarden@gmail.com>
2023-12-08 00:45:23 +0800

    2023-12-08 00:45:23

--- main.py
+++ main.py
@@ -69,3 +69,4 @@ static test() {
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
