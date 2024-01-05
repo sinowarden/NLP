e5d2fede6258690eb69c7ad45a2bcbbeaa29bfe8
sinowarden <sinowarden@gmail.com>
2024-01-05 23:51:16 +0800

    2024-01-05 23:51:16

--- main.py
+++ main.py
@@ -113,3 +113,4 @@ static test() {
   schedule(DoNothingTest.do_nothing_grinder_test1);
   schedule(DoNothingTest.do_nothing_grinder_test1b);
   schedule(DoNothingTest.do_nothing_grinder_test2);
+  schedule(DoNothingTest.do_nothing_variable_test);
