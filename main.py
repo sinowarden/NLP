a51cc0b500a471d20addd25b1e2188c45fa7324e
sinowarden <sinowarden@gmail.com>
2024-06-18 02:28:51 +0800

    2024-06-18 02:28:51

--- main.py
+++ main.py
@@ -203,3 +203,4 @@ const Vector = goog.require('myphysicslab.lab.util.Vector');
 // distributed under the License is distributed on an 'AS IS' BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
+// limitations under the License.
