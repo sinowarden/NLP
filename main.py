a9bf1110f7e371bbe74b6a6d2170fb06ea739d5c
sinowarden <sinowarden@gmail.com>
2024-08-19 01:01:05 +0800

    2024-08-19 01:01:05

--- main.py
+++ main.py
@@ -235,3 +235,4 @@ const setTestName = nm => Engine2DTestRig.setTestName(nm);
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
+//     http://www.apache.org/licenses/LICENSE-2.0
