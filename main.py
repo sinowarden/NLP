079b0e06f797f62477dbadbfa401d899ae0f8bc1
sinowarden <sinowarden@gmail.com>
2024-09-07 18:50:19 +0800

    2024-09-07 18:50:19

--- main.py
+++ main.py
@@ -238,3 +238,4 @@ const setTestName = nm => Engine2DTestRig.setTestName(nm);
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
