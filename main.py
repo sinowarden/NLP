b8474cac1c3718f25f5322684f81c5518dfcb89d
sinowarden <sinowarden@gmail.com>
2024-11-22 23:53:45 +0800

    2024-11-22 23:53:45

--- main.py
+++ main.py
@@ -323,3 +323,4 @@ static do_nothing_variable_test() {
   const advance = new CollisionAdvance(sim);
   DoNothingTest.do_nothing_variable_setup(sim, advance);
   const vars = makeVars((4 + 3)*6);
+  Engine2DTestRig.setBodyVars(sim, vars, 0, -2.350918, -1.4252083, 0.2181222, -0.3159406, -7.2797567, -0.9370725);
