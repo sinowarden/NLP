524fb0a88623cdc9e3a951290c4b511fe0035093
sinowarden <sinowarden@gmail.com>
2024-11-25 01:11:55 +0800

    2024-11-25 01:11:54

--- main.py
+++ main.py
@@ -326,3 +326,4 @@ static do_nothing_variable_test() {
   Engine2DTestRig.setBodyVars(sim, vars, 0, -2.350918, -1.4252083, 0.2181222, -0.3159406, -7.2797567, -0.9370725);
   Engine2DTestRig.setBodyVars(sim, vars, 1, 0, -0, 1.7390377, -2.5189213, 3.1415927, -0);
   Engine2DTestRig.setBodyVars(sim, vars, 2, -2.688075, -1.6296046, -0, 0, 1.5707963, 0);
+  setBodyVars(sim, vars, 3, 2.507, 0, 2.507, 0, 0, 0);
