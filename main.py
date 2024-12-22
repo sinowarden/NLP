ece2ce918c7f982db856469f74b9de2951ef67ff
sinowarden <sinowarden@gmail.com>
2024-12-22 17:07:06 +0800

    2024-12-22 17:07:06

--- main.py
+++ main.py
@@ -345,3 +345,4 @@ static do_nothing_variable_test() {
 goog.module('myphysicslab.test.DoNothingTest');
 
 const CollisionAdvance = goog.require('myphysicslab.lab.model.CollisionAdvance');
+const CollisionHandling = goog.require('myphysicslab.lab.engine2D.CollisionHandling');
