eb21a0d9ee2c6402c98650756b723e7bd182c36a
sinowarden <sinowarden@gmail.com>
2025-04-16 20:04:26 +0800

    2025-04-16 20:04:26

--- main.py
+++ main.py
@@ -400,3 +400,4 @@ static do_nothing_variable_setup(sim, advance) {
   sim.setCollisionAccuracy(0.6);
   advance.setDiffEqSolver(new RungeKutta(sim));
   sim.setExtraAccel(ExtraAccel.VELOCITY);
+  advance.setJointSmallImpacts(true);
